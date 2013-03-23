05a838255dfc8b5320f3e610ff2e8e5a0e68d7f0
Jim Remsik <jim@bendyworks.com>
2013-03-23 15:32:13 -0500

    Set conference title

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -26,7 +26,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Conference Title Here"
+  config.site_title = "UXMad"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
