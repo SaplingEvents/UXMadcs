97731156a74f3a619d88f61c657dd6e5cc82f879
Jim Remsik <jim@bendyworks.com>
2013-03-24 20:18:09 -0500

    Make CarrierWave work on Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,9 @@
 CarrierWave.configure do |config|
-  # config.permissions = 0666
-  # config.directory_permissions = 0777
-  #config.storage = :file
-
   if Rails.env.test? or Rails.env.development?
     config.storage :file
     config.enable_processing = false
   else
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.storage :fog
   end
 
@@ -15,5 +12,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
-  config.fog_directory  = 'confskel'
+  config.fog_directory  = 'uxmad'
 end
