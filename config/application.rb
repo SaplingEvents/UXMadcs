5dc4aa521b16254163cab2db58d364f8951441dd
Jim Remsik <jim@thunderboltlabs.com>
2013-11-21 22:20:29 -0600

    No initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Confskel
       Idkfa.load_keys Rails.env, credentials: Rails.root.join('config/credentials.yml')
     end
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
