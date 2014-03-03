58233a084dc233609bc68bfa599afd2c97a786f8
Jim Remsik <jremsikjr@gmail.com>
2014-03-03 11:02:59 -0600

    Dev wouldn't work without this

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,6 @@ module Confskel
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.middleware.use Floodgate::Control, ENV['FLOODGATE_APP_ID'], ENV['FLOODGATE_API_TOKEN']
   end
 end
